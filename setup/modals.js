bb5874fadd67c726b6f48d3b12c1ac75953d828e
devhgv <tech.hgv@gmail.com>
2024-12-04 12:29:16 -0300

    atualização1 dev

--- setup/modals.js
+++ setup/modals.js
@@ -2,7 +2,7 @@
 const titleModalAboutProducts = "Sobre o módulo de Produtos";
 const titleModalAboutTests = "Sobre o relatório de Testes";
 const messageModalAbout =
-  "Este módulo tem como propósito fornecer dados sobre os produtos e seus valores de categoria. Espera-se que este módulo faça uma requisição dos dados dos produtos e que todos sejam renderizados corretamente na planilha. Não se espera que nenhum dado esteja incorreto, mas é possível que alguns valores sejam de fatos nulos ou vazios.";
+  "Este módulo tem como propósito fornecer dados sobre os produtos. Espera-se que este módulo faça uma requisição dos dados dos produtos e que todos sejam renderizados corretamente na planilha. Não se espera que nenhum dado esteja incorreto, mas é possível que alguns valores sejam de fatos nulos ou vazios.";
 const messageModalAboutTests = 'Este relatório tem como propósito fornecer os resultados dos testes do módulo de produtos. Espera-se que os testes forneçam resultados satisfatórios, de preferência a coluna Resultados deve possuir todos os valores APROVADOS';
 
 // FUNCÕES MODAIS
